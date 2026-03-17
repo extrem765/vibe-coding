30009901d0f58bc375622f20263f1fb0778a398e
extrem765 <bojkiv1989@gmail.com>
2026-03-17 18:40:50 +0200

    fix bag

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true, index: true },
+  username: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
 }, { timestamps: true });
 
